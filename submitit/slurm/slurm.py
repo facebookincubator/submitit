6ab85bff070a826ccc063447eea136c96aed9a0e
QuentinDuval <QuentinDuval@users.noreply.github.com>
2020-06-03 03:02:20 -0700

    Fix review: Remove @staticmethod and replace by private methods

--- submitit/slurm/slurm.py
+++ submitit/slurm/slurm.py
@@ -124,6 +124,55 @@ class SlurmParseException(Exception):
     pass
 
 
+def _expand_id_suffix(suffix_parts: str) -> List[str]:
+    """Parse the a suffix formatted like "1-3,5,8" into
+    the list of numeric values 1,2,3,5,8.
+    """
+    suffixes = []
+    for suffix_part in suffix_parts.split(","):
+        if "-" in suffix_part:
+            low, high = suffix_part.split("-")
+            for num in range(int(low), int(high) + 1):
+                suffixes.append(str(num))
+        else:
+            suffixes.append(suffix_part)
+    return suffixes
+
+
+def _parse_node_group(node_list: str, pos: int, parsed: List[str]) -> int:
+    """Parse a node group of the form PREFIX[1-3,5,8] and return
+    the position in the string at which the parsing stopped
+    """
+    prefixes = [""]
+    while pos < len(node_list):
+        c = node_list[pos]
+        if c == ",":
+            parsed.extend(prefixes)
+            return pos + 1
+        if c == "[":
+            last_pos = node_list.index("]", pos)
+            suffixes = _expand_id_suffix(node_list[pos + 1 : last_pos])
+            prefixes = [prefix + suffix for prefix in prefixes for suffix in suffixes]
+            pos = last_pos + 1
+        else:
+            for i, prefix in enumerate(prefixes):
+                prefixes[i] = prefix + c
+            pos += 1
+    parsed.extend(prefixes)
+    return pos
+
+
+def _parse_node_list(node_list: str):
+    try:
+        pos = 0
+        parsed: List[str] = []
+        while pos < len(node_list):
+            pos = _parse_node_group(node_list, pos, parsed)
+        return parsed
+    except ValueError as e:
+        raise SlurmParseException(f"Unrecognized format for SLURM_JOB_NODELIST: '{node_list}'", e)
+
+
 class SlurmJobEnvironment(job_environment.JobEnvironment):
     _env = {
         "job_id": "SLURM_JOB_ID",
@@ -160,52 +209,7 @@ class SlurmJobEnvironment(job_environment.JobEnvironment):
         node_list = os.environ.get(self._env["nodes"], "")
         if not node_list:
             return [self.hostname]
-
-        try:
-            pos = 0
-            parsed: List[str] = []
-            while pos < len(node_list):
-                pos = self._parse_group(node_list, pos, parsed)
-            return parsed
-        except ValueError as e:
-            raise SlurmParseException(f"Unrecognized format for SLURM_JOB_NODELIST: '{node_list}'", e)
-
-    def _parse_group(self, node_list: str, pos: int, parsed: List[str]) -> int:
-        """Parse a suffix group of the form PREFIX[1-3,5,8] and return
-        the position in the string at which the parsing stopped
-        """
-        prefixes = [""]
-        while pos < len(node_list):
-            c = node_list[pos]
-            if c == ",":
-                parsed.extend(prefixes)
-                return pos + 1
-            if c == "[":
-                last_pos = node_list.index("]", pos)
-                suffixes = self._expand_suffix(node_list[pos + 1 : last_pos])
-                prefixes = [prefix + suffix for prefix in prefixes for suffix in suffixes]
-                pos = last_pos + 1
-            else:
-                for i, prefix in enumerate(prefixes):
-                    prefixes[i] = prefix + c
-                pos += 1
-        parsed.extend(prefixes)
-        return pos
-
-    @staticmethod
-    def _expand_suffix(suffix_parts: str) -> List[str]:
-        """Parse the a suffix formatted like "1-3,5,8" into
-        the list of numeric values 1,2,3,5,8.
-        """
-        suffixes = []
-        for suffix_part in suffix_parts.split(","):
-            if "-" in suffix_part:
-                low, high = suffix_part.split("-")
-                for num in range(int(low), int(high) + 1):
-                    suffixes.append(str(num))
-            else:
-                suffixes.append(suffix_part)
-        return suffixes
+        return _parse_node_list(node_list)
 
 
 class SlurmExecutor(core.PicklingExecutor):
