19a7d643e110c858e426e759928b3f277f1c3305
lematt1991 <lematt1991@gmail.com>
2021-01-13 06:50:53 -0800

    use rsync instead of explicitly iterating over files in python

--- submitit/snapshot.py
+++ submitit/snapshot.py
@@ -2,7 +2,7 @@ import fnmatch
 import os
 import shutil
 from subprocess import check_output
-
+from tempfile import NamedTemporaryFile
 
 def run_cmd(str_args, **kwargs):
     return check_output(str_args, shell=True, **kwargs).decode("utf-8").strip()
@@ -23,15 +23,11 @@ class SnapshotManager:
         # Get the repository root
         root_dir = run_cmd("git rev-parse --show-toplevel")
         sub = "--recurse-submodules" if self.with_submodules else "-s"
-        # https://stackoverflow.com/a/51689219/4876946
-        files = run_cmd(f"git ls-files {sub} | grep -v ^16 | cut -f2-", cwd=root_dir).split("\n")
-        # Copy all the files into the snapshot directory
-        for file in files:
-            if any([fnmatch.fnmatch(file, ex) for ex in self.exclude]):
-                continue
-            dest_file = os.path.join(self.snapshot_dir, file)
-            os.makedirs(os.path.dirname(dest_file), exist_ok=True)
-            shutil.copy(os.path.join(root_dir, file), dest_file)
+        # Get a list of all the checked in files that we can pass to rsync
+        with NamedTemporaryFile() as tfile:
+            # https://stackoverflow.com/a/51689219/4876946
+            files = run_cmd(f"git ls-files {sub} | grep -v ^16 | cut -f2- > {tfile.name}", cwd=root_dir).split("\n")
+            run_cmd(f"rsync -a --files-from={tfile.name} {root_dir} {self.snapshot_dir}")
         os.chdir(self.snapshot_dir)
 
     def __exit__(self, *args):
