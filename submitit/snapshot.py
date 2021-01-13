04f26edbf82f09a5b99af7d78db9eb129635ddb9
lematt1991 <lematt1991@gmail.com>
2021-01-13 07:20:56 -0800

    addressing @gwenzek's comments

--- submitit/snapshot.py
+++ submitit/snapshot.py
@@ -1,33 +1,59 @@
-import fnmatch
+import itertools
 import os
-import shutil
-from subprocess import check_output
-from tempfile import NamedTemporaryFile
+import subprocess
+import tempfile
+from pathlib import Path
+from typing import List, Optional
+
 
 def run_cmd(str_args, **kwargs):
-    return check_output(str_args, shell=True, **kwargs).decode("utf-8").strip()
+    return subprocess.check_output(str_args, **kwargs).decode("utf-8").strip()
 
 
 class SnapshotManager:
+    """Snapshot Manager
+    This class creates a snapshot of the git repository that the script lives in
+    when creating the snapshot.  This is useful for ensuring that remote jobs that
+    get launched don't accidentally pick up unintended local changes.
+
+    Parameters
+    ----------
+    snapshot_dir: Path
+        A path to where the snapshot should be created
+    with_submodules: bool
+        Whether or not submodules should be included in the snapshot
+    exclude: Optional[List[str]]
+        An optional list of patterns to exclude from the snapshot
+
+    Note
+    ----
+    - Only files that are checked in to the repository are included in the snapshot.
+        If you have experimental code that you would like to include in the snapshot,
+        you'll need to `git add` the file first for it to be included
+    """
+
     def __init__(
-        self, snapshot_dir: str, with_submodules: bool = False, exclude=None,
+        self, snapshot_dir: Path, with_submodules: bool = False, exclude: Optional[List[str]] = None,
     ):
         self.snapshot_dir = snapshot_dir
-        self.original_dir = os.getcwd()
+        self.original_dir = Path.cwd()
         self.with_submodules = with_submodules
         self.exclude = exclude or []
 
     def __enter__(self):
-        self.original_dir = os.getcwd()
-        os.makedirs(self.snapshot_dir, exist_ok=True)
+        self.original_dir = Path.cwd()
+        self.snapshot_dir.mkdir(parents=True, exist_ok=True)
         # Get the repository root
-        root_dir = run_cmd("git rev-parse --show-toplevel")
+        root_dir = run_cmd(["git", "rev-parse", "--show-toplevel"])
         sub = "--recurse-submodules" if self.with_submodules else "-s"
         # Get a list of all the checked in files that we can pass to rsync
-        with NamedTemporaryFile() as tfile:
+        with tempfile.NamedTemporaryFile() as tfile:
             # https://stackoverflow.com/a/51689219/4876946
-            files = run_cmd(f"git ls-files {sub} | grep -v ^16 | cut -f2- > {tfile.name}", cwd=root_dir).split("\n")
-            run_cmd(f"rsync -a --files-from={tfile.name} {root_dir} {self.snapshot_dir}")
+            run_cmd(
+                f"git ls-files {sub} | grep -v ^16 | cut -f2- > {tfile.name}", cwd=root_dir, shell=True
+            )
+            exclude = list(itertools.chain(*[["--exclude", pat] for pat in self.exclude]))
+            run_cmd(["rsync", "-a", f"--files-from={tfile.name}", root_dir, self.snapshot_dir] + exclude)
         os.chdir(self.snapshot_dir)
 
     def __exit__(self, *args):
--- submitit/test_snapshot.py
+++ submitit/test_snapshot.py
@@ -1,13 +1,27 @@
 import os
-from tempfile import TemporaryDirectory
+from pathlib import Path
 
+from .local import local
 from .snapshot import SnapshotManager
 
 
-def test_snapshot():
-    cwd = os.getcwd()
-    with TemporaryDirectory() as tdir:
-        with SnapshotManager(tdir):
-            assert os.getcwd() == tdir
-            assert os.path.exists(os.path.join(tdir, "submitit/test_snapshot.py"))
-    assert os.getcwd() == cwd
+def test_snapshot(tmp_path):
+    cwd = Path.cwd()
+    with SnapshotManager(tmp_path):
+        assert Path.cwd() == tmp_path
+        assert (tmp_path / "submitit/test_snapshot.py").exists()
+    assert Path.cwd() == cwd
+
+
+def test_exclude(tmp_path):
+    exclude = ["submitit/test_*"]
+    with SnapshotManager(snapshot_dir=tmp_path, exclude=exclude):
+        assert (tmp_path / "submitit/snapshot.py").exists()
+        assert not (tmp_path / "submitit/test_snapshot.py").exists()
+
+
+def test_submitted_job(tmp_path):
+    executor = local.LocalExecutor(tmp_path)
+    with SnapshotManager(snapshot_dir=tmp_path):
+        job = executor.submit(os.getcwd)
+        assert Path(job.result()) == tmp_path
