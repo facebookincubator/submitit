681b06775902d0e3369eb7c0c02eeaeb398995d5
Guillaume Wenzek <gwenzek@users.noreply.github.com>
2021-01-25 22:01:37 +0100

    restrict test_documentation to two folders for faster test

--- submitit/test_documentation.py
+++ submitit/test_documentation.py
@@ -6,22 +6,7 @@
 
 import re
 from pathlib import Path
-from typing import List, Union
-
-
-def assert_markdown_links_not_broken(folder: Union[str, Path]) -> None:
-    """Asserts that all relative hyperlinks are valid in markdown files of the folder
-    and its subfolders.
-
-    Note
-    ----
-    http hyperlinks are not tested.
-    """
-    links = _get_all_markdown_links(folder)
-    broken = [l for l in links if not l.exists()]
-    if broken:
-        text = "\n - ".join([str(l) for l in broken])
-        raise AssertionError(f"Broken markdown links:\n - {text}")
+from typing import List
 
 
 class _MarkdownLink:
@@ -29,42 +14,52 @@ class _MarkdownLink:
     (external links are not tested)
     """
 
-    def __init__(self, folder: Path, filepath: Path, string: str, link: str) -> None:
-        self._folder = folder
-        self._filepath = filepath
+    def __init__(self, root: Path, file: Path, string: str, link: str) -> None:
+        self._root = root
+        self._file = file
         self._string = string
         self._link = link
 
     def exists(self) -> bool:
-        if self._link.startswith("http"):  # consider it exists
+        if self._link.startswith("http"):
+            # We don't check external urls.
             return True
         link = self._link.split("#")[0]
         if not link:
-            return True
-        fullpath = self._folder / self._filepath.parent / link
+            return False
+        fullpath = self._root / self._file.parent / link
         return fullpath.exists()
 
     def __repr__(self) -> str:
-        return f"{self._link} ({self._string}) from file {self._filepath}"
+        return f"[{self._link}]({self._string}) in file {self._file}"
 
 
-def _get_all_markdown_links(folder: Union[str, Path]) -> List[_MarkdownLink]:
-    """Returns a list of all existing markdown links
-    """
+def _get_root() -> Path:
+    root = Path(__file__).parent.parent.absolute()
+    assert (root / "setup.py").exists(), f"Wrong root folder: {root}"
+    return root
+
+
+def _get_markdown_files(root: Path) -> List[Path]:
+    return [md for pattern in ("*.md", "submitit/**/*.md", "docs/**/*.md") for md in root.glob(pattern)]
+
+
+def _get_all_markdown_links(root: Path, files: List[Path]) -> List[_MarkdownLink]:
+    """Returns a list of all existing markdown links"""
     pattern = re.compile(r"\[(?P<string>.+?)\]\((?P<link>\S+?)\)")
-    folder = Path(folder).expanduser().absolute()
     links = []
-    for rfilepath in folder.glob("**/*.md"):
-        filepath = folder / rfilepath
-        with filepath.open("r") as f:
-            text = f.read()
-        for match in pattern.finditer(text):
-            links.append(_MarkdownLink(folder, rfilepath, match.group("string"), match.group("link")))
+    for file in files:
+        for match in pattern.finditer(file.read_text()):
+            links.append(_MarkdownLink(root, file, match.group("string"), match.group("link")))
     return links
 
 
 def test_assert_markdown_links_not_broken() -> None:
-    folder = Path(__file__).parent.parent.absolute()
-    assert (folder / "setup.py").exists(), f"Wrong root folder: {folder}"
-    assert _get_all_markdown_links(folder), "There should be at least one hyperlink!"
-    assert_markdown_links_not_broken(folder)
+    root = _get_root()
+    files = _get_markdown_files(root)
+    assert len(files) > 3
+
+    links = _get_all_markdown_links(root, files)
+    assert len(links) > 5, "There should be several hyperlinks!"
+    broken_links = [l for l in links if not l.exists()]
+    assert not broken_links
