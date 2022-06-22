8716a561d2e59a2075eb83d937d5e149e443ab70
Tom McTiernan <tmct@users.noreply.github.com>
2022-06-22 16:06:07 +0100

    Run formatter

--- submitit/core/core.py
+++ submitit/core/core.py
@@ -669,7 +669,7 @@ class Executor(abc.ABC):
     def name(cls) -> str:
         n = cls.__name__
         if n.endswith("Executor"):
-            n = n[:-len("Executor")]
+            n = n[: -len("Executor")]
         return n.lower()
 
     @contextlib.contextmanager
