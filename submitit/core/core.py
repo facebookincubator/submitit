8d8e46586b6cfeef8ffc741efb41ccfdaf578b04
Deniz Oktay <1954448+denizokt@users.noreply.github.com>
2024-08-22 08:58:35 -0700

    Consider PREEMPTED state as "not done"
    Currently if a job is preempted, submitit considers the job as failed, because it does not find the pickle file and PREEMPTED is currently considered to signal that a job is "done".
    
    In reality it will either requeue (in which case it is not done) or it will eventually transition to CANCELLED (in which case submitit will detect it later anyway.

--- submitit/core/core.py
+++ submitit/core/core.py
@@ -100,7 +100,7 @@ class InfoWatcher:
             one of "force" (forces a call), "standard" (calls regularly) or "cache" (does not call)
         """
         state = self.get_state(job_id, mode=mode)
-        return state.upper() not in ["READY", "PENDING", "RUNNING", "UNKNOWN", "REQUEUED", "COMPLETING"]
+        return state.upper() not in ["READY", "PENDING", "RUNNING", "UNKNOWN", "REQUEUED", "COMPLETING", "PREEMPTED"]
 
     def update_if_long_enough(self, mode: str) -> None:
         """Updates if forced to, or if the delay is reached
