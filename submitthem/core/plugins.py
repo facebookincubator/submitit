6f53722294843845197f3380fa3796aef31e78e6
Xavier Roynard <xavier.roynard@safrangroup.com>
2025-12-04 21:38:03 +0100

    keep refactoring

--- submitthem/core/plugins.py
+++ submitthem/core/plugins.py
@@ -25,11 +25,12 @@ def _get_plugins() -> Tuple[List[Type["Executor"]], List["JobEnvironment"]]:
 
     from ..local import debug, local
     from ..slurm import slurm
+    from ..pbs import pbs
 
     # TODO: use sys.modules.keys() and importlib.resources to find the files
     # We load both kind of entry points at the same time because we have to go through all module files anyway.
-    executors: List[Type["Executor"]] = [slurm.SlurmExecutor, local.LocalExecutor, debug.DebugExecutor]
-    job_envs = [slurm.SlurmJobEnvironment(), local.LocalJobEnvironment(), debug.DebugJobEnvironment()]
+    executors: List[Type["Executor"]] = [slurm.SlurmExecutor, pbs.PBSExecutor, local.LocalExecutor, debug.DebugExecutor]
+    job_envs = [slurm.SlurmJobEnvironment(), pbs.PBSJobEnvironment(), local.LocalJobEnvironment(), debug.DebugJobEnvironment()]
     for entry_point in pkg_resources.iter_entry_points("submitthem"):
         if entry_point.name not in ("executor", "job_environment"):
             logger.warning(f"Found unknown entry point in package {entry_point.module_name}: {entry_point}")
